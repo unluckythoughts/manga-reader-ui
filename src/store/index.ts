629a437572e3d607538703f6bfc28b84b8a412b8
Unlucky Thoughts <gvsmraju@gmail.com>
2022-09-03 11:19:17 -0400

    Added vuex persist for the local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { createLogger, createStore } from "vuex"
+import VuexPersistence from "vuex-persist"
 import { actions } from "./actions"
 import { connector } from "./connector"
 import { getters } from "./getters"
 import { library } from "./library"
-import { mutations } from "./mutations"
+import { mutations, MutationTypes } from "./mutations"
 import { Manga, State } from "./types"
 
 const state: State = {
@@ -14,17 +15,21 @@ const state: State = {
   currentManga: new Manga()
 }
 
-// const vuexLocal = new VuexPersistence<State>({
-//   key: "manga-reader",
-//   storage: window.localStorage,
-//   filter(mutation) {
-//     if (mutation.type === MutationTypes.SET_LOADING) {
-//       return false
-//     }
+const vuexLocal = new VuexPersistence<State>({
+  key: "manga-reader",
+  storage: window.localStorage,
+  filter(mutation) {
+    if (
+      mutation.type === MutationTypes.SET_READER_MODE ||
+      mutation.type === MutationTypes.SET_LOADING ||
+      mutation.type === MutationTypes.SET_IN_LIBRARY
+    ) {
+      return false
+    }
 
-//     return true
-//   }
-// })
+    return true
+  }
+})
 
 export const store = createStore({
   strict: true,
@@ -36,5 +41,5 @@ export const store = createStore({
     library,
     connector
   },
-  plugins: [createLogger()]
+  plugins: [createLogger(), vuexLocal.plugin]
 })
