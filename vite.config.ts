e9d400b20a53f5e2a7dbcca0ab12082928247297
Unlucky Thoughts <gvsmraju@gmail.com>
2023-07-22 19:22:27 -0400

    chore(build): moved images buiid dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv, UserConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  return {
+  let config: UserConfig = {
     define: {
       __APP_ENV__: env.APP_ENV
     },
     plugins: [
       vue(),
+      viteStaticCopy({
+        targets: [{
+          src: "public/**/*.(svg|png|jpe?g|gif)",
+          dest: "assets/images"
+        }, {
+          src: "public/favicon.ico",
+          dest: ""
+        }]
+      })
     ],
     resolve: {
       alias: {
@@ -20,11 +29,13 @@ export default defineConfig(({ command, mode }) => {
     },
     build: {
       outDir: env.UI_OUT_DIR || "dist",
+      copyPublicDir: false,
+      emptyOutDir: true,
       rollupOptions: {
         output: {
           assetFileNames: ({ name }) => {
             if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-              return 'assets/images/[name]-[hash][extname]';
+              return "assets/images/[name]-[hash][extname]"
             }
 
             return "assets/[name]-[hash][extname]"
@@ -33,4 +44,7 @@ export default defineConfig(({ command, mode }) => {
       }
     }
   }
+
+  console.log(config.build?.rollupOptions?.input)
+  return config
 })
