74d1fed42dd6545be678d762bf0bf90a8e1d02a3
Unlucky Thoughts <gvsmraju@gmail.com>
2023-07-22 18:24:49 -0400

    change(build)!: updated build dir to env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      outDir: env.UI_OUT_DIR || "dist"
     }
   }
 })
